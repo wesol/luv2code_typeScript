39932103fd5ccab2d0fe9290db98b46e720fd768
Mateusz Wesołowski <mateusz.wesolowski@eyzee.com>
2019-12-03 16:28:41 +0100

    Added variables files

---
+++ 02-variables/sample-types.js
@@ -0,0 +1,6 @@
+var found = true;
+var grade = 88.6;
+var firstName;
+var lastName;
+found = 0;
+grade = "nn";
---
+++ 02-variables/sample-types.ts
@@ -0,0 +1,6 @@
+let found: boolean = true;
+let grade: number = 88.6;
+let firstName: "Name";
+let lastName: "Surname";
+
+console.log(firstName)
