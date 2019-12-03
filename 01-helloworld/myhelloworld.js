26cd7a92dfbd481ea7ea96b07296db1f8b9817b1
Mateusz Wesołowski <mateusz.wesolowski@eyzee.com>
2019-12-03 16:33:43 +0100

    Removed js files

--- 01-helloworld/myhelloworld.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello World");
-console.log("Coding Gurus! " + "Time for some fun!");
--- 02-variables/sample-types.js
+++
@@ -1,6 +0,0 @@
-var found = true;
-var grade = 88.6;
-var firstName;
-var lastName;
-found = 0;
-grade = "nn";
